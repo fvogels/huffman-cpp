d352918ab8dccfbced0c527be7760372bb559a5a
Frederic Vogels <frederic.vogels@ucll.be>
2021-03-20 12:10:57 +0100

    group

--- python/huffman.py
+++ python/huffman.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import Any, TypeVar, Union, Literal, Generic, Callable
+from math import ceil
 
 
 T = TypeVar('T')
@@ -8,3 +9,8 @@ T = TypeVar('T')
 def pad(xs : list[T], length : int, padder : T) -> list[T]:
     padding = [ padder ] * (length - len(xs)) # Check what [x] * -5 does
     return [ *xs, *padding ]
+
+
+def group(xs : list[T], group_size : int) -> list[list[T]]:
+    ngroups = ceil(len(xs) / group_size)
+    return [ xs[i * group_size : (i+1) * group_size] for i in range(ngroups) ]
--- python/tests.py
+++ python/tests.py
@@ -12,3 +12,12 @@ def test_padding():
     yield check, [5, 5], [5, 5], 2, 1
     yield check, [5, 5, 3], [5, 5, 3], 2, 1
     yield check, [5, 5, 3, 0, 0], [5, 5, 3], 5, 0
+
+
+def test_group():
+    def check(expected, xs, group_size):
+        assert expected == group(xs, group_size)
+    yield check, [[1], [2], [3]], [1, 2, 3], 1
+    yield check, [[1, 2], [3]], [1, 2, 3], 2
+    yield check, [[1, 2], [3, 4]], [1, 2, 3, 4], 2
+    yield check, [[1, 2], [3, 4], [5, 6]], [1, 2, 3, 4, 5, 6], 2

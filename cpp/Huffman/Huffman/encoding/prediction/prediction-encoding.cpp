6ba13fe8042c831d7fd9c8c2c450b17ffbf077ff
Frederic Vogels <frederic.vogels@ucll.be>
2021-04-06 19:06:12 +0200

    prediction encoding

---
+++ cpp/Huffman/Huffman/encoding/prediction/prediction-encoding.cpp
@@ -0,0 +1,71 @@
+#include "encoding/prediction/prediction-encoding.h"
+#include <assert.h>
+
+
+namespace
+{
+    class PredictiveEncodingImplementation : encoding::EncodingImplementation
+    {
+    private:
+        u64 m_domain_size;
+        std::unique_ptr<encoding::prediction::Oracle> m_oracle;
+
+    public:
+        PredictiveEncodingImplementation(u64 domain_size, std::unique_ptr<encoding::prediction::Oracle> oracle) : m_domain_size(domain_size), m_oracle(std::move(oracle))
+        {
+            // NOP
+        }
+
+        void encode(io::InputStream& input, io::OutputStream& output) const override
+        {
+            while (!input.end_reached())
+            {
+                auto actual_datum = input.read();
+                auto predicted_datum = m_oracle->predict();
+                auto correction = correct(actual_datum, predicted_datum);
+                output.write(correction);
+            }
+        }
+
+        void decode(io::InputStream& input, io::OutputStream& output) const override
+        {
+            while (!input.end_reached())
+            {
+                auto correction = input.read();
+                auto predicted_datum = m_oracle->predict();
+                auto datum = apply_correction(predicted_datum, correction);
+                output.write(datum);
+            }
+        }
+
+    private:
+        Datum correct(Datum actual_datum, Datum predicted_datum) const
+        {
+            assert(actual_datum < m_domain_size);
+            assert(predicted_datum < m_domain_size);
+
+            if (actual_datum >= predicted_datum)
+            {
+                return actual_datum - predicted_datum;
+            }
+            else
+            {
+                return m_domain_size - predicted_datum + actual_datum;
+            }
+        }
+
+        Datum apply_correction(Datum predicted_datum, Datum correction) const
+        {
+            assert(predicted_datum < m_domain_size);
+            assert(correction < m_domain_size);
+
+            assert(predicted_datum + correction < predicted_datum);
+            return (predicted_datum + correction) % m_domain_size;
+        }
+    };
+}
+
+std::shared_ptr<encoding::EncodingImplementation> create_predictive_encoding_implementation(u64 domain_size, std::unique_ptr<encoding::prediction::Oracle> oracle)
+{
+    return std::make_shared<encoding::EncodingImplementation>(domain_size, std::move(oracle));
+}
--- cpp/Huffman/Huffman/encoding/prediction/prediction-encoding.h
+++ cpp/Huffman/Huffman/encoding/prediction/prediction-encoding.h
@@ -8,10 +8,10 @@
 
 namespace encoding
 {
-    std::shared_ptr<EncodingImplementation> create_predictive_encoding_implementation(u64 domain_size);
+    std::shared_ptr<EncodingImplementation> create_predictive_encoding_implementation(std::unique_ptr<encoding::prediction::Oracle> oracle);
 
-    template<u64 IN>
-    Encoding<IN, 2> create_predictive_encoder(std::unique_ptr<encoding::prediction::Oracle> oracle)
+    template<u64 N>
+    Encoding<N, N> create_predictive_encoder(std::unique_ptr<encoding::prediction::Oracle> oracle)
     {
         return encoding::Encoding<IN, 2>(create_predictive_encoding_implementation(IN, std::move(oracle));
     }

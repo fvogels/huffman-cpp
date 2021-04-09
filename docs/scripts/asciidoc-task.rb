65b3b11ea5444ee213282bb1a6e293ada5be76a8
Frederic Vogels <frederic.vogels@ucll.be>
2021-04-09 17:20:12 +0200

    docs beginnings

---
+++ docs/scripts/asciidoc-task.rb
@@ -0,0 +1,34 @@
+require 'asciidoctor'
+require 'asciidoctor/extensions'
+
+
+class TaskBlock < Asciidoctor::Extensions::BlockProcessor
+  use_dsl
+  named :TASK
+  on_context :example
+
+  def process(parent, reader, attrs)
+    attrs = {**attrs, "name" => 'task', "caption" => 'test'}
+    create_block(parent, :admonition, reader.readlines, attrs, content_model: :compound)
+  end
+end
+
+class TaskBlockDocinfoProcessor < Asciidoctor::Extensions::DocinfoProcessor
+  use_dsl
+
+  def process doc
+    <<~END
+      <style>
+        .admonitionblock.task td.icon .title::before {
+          content: "🔨";
+          color: #871452;
+        }
+      </style>'
+    END
+  end
+end
+
+Asciidoctor::Extensions.register do
+  block TaskBlock
+  docinfo_processor TaskBlockDocinfoProcessor if document.basebackend? 'html'
+end

3d590a5d0fd5e7e0158f92fd7269f54c4e3572c5
Frederic Vogels <frederic.vogels@ucll.be>
2021-04-15 12:37:06 +0200

    test splitting

--- python/tests/tests.py
+++ python/tests/tests.py
@@ -17,31 +17,6 @@ from bit_grouper import BitGrouperEncoding
 from oracles import MemoryOracle, MarkovOracle, ConstantOracle, RepeatOracle
 
 
-def test_predictions():
-    def check(data, oracle_factory):
-        encoding = PredictionEncoding(oracle_factory, 256)
-        encoded = encoding.encode(data)
-        restored = list(encoding.decode(encoded))
-        assert restored == data, f'{restored} != {data}'
-
-    oracles = [
-        lambda: ConstantOracle(0),
-        lambda: RepeatOracle(0),
-        lambda: MarkovOracle(0),
-        lambda: MemoryOracle(1, 0),
-        lambda: MemoryOracle(2, 0),
-        lambda: MemoryOracle(3, 0),
-    ]
-
-    for oracle in oracles:
-        yield check, [0], oracle
-        yield check, [0, 1], oracle
-        yield check, [0, 1, 2, 3, 4], oracle
-        yield check, [5, 1, 2, 9, 4], oracle
-        yield check, [255, 128, 254, 0], oracle
-        yield check, [1, 1, 1, 1, 1, 1, 1, 1], oracle
-
-
 def test_burrows_wheeler():
     def check(data, nvalues):
         encoding = BurrowsWheeler(nvalues)

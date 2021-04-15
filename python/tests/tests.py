53aa3c877fdd59ccf6a124c8e320d33bf4e5d829
Frederic Vogels <frederic.vogels@ucll.be>
2021-04-15 12:32:56 +0200

    test splitting

--- python/tests/tests.py
+++ python/tests/tests.py
@@ -153,20 +153,7 @@ def test_encoder_combination():
         yield check, encoding, [1, 2, 3] * 10
 
 
-def test_bit_grouper():
-    def check(bits, group_size):
-        encoding = BitGrouperEncoding(group_size)
-        encoded = encoding.encode(bits)
-        decoded = list(encoding.decode(encoded))[0:len(bits)]
-        assert bits == decoded, f'bits={bits}, decoded={decoded}'
 
-    for group_size in [2, 4, 8, 12, 16]:
-        yield check, [], group_size
-        yield check, [0], group_size
-        yield check, [1], group_size
-        yield check, [1, 1], group_size
-        yield check, [1, 0, 1, 0, 1, 1, 1, 0], group_size
-        yield check, [0] * 8 * 10, group_size
 
 
 def test_eof_encoding():

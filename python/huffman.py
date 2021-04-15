7187fb9ef54644c790103917d377718ce075c10f
Frederic Vogels <frederic.vogels@ucll.be>
2021-04-15 12:10:58 +0200

    moved python tests to subfolder

--- python/huffman.py
+++ python/huffman.py
@@ -65,36 +65,3 @@ class HuffmanEncoding(Encoding[Data, Iterable[Bit]]):
         tree : Node[Datum] = self.__tree_encoding.decode(iterator)
         decoded = decode_data(iterator, tree)
         return decoded
-
-
-
-
-class FullTreeAdaptiveHuffmanEncoding(Encoding[Data, Iterable[Bit]]):
-    __nvalues : int
-
-    def __init__(self, nvalues : int):
-        assert nvalues > 1
-        self.__nvalues = nvalues
-
-    def encode(self, data : Data) -> Iterable[Bit]:
-        frequencies : FrequencyTable[Datum] = FrequencyTable.with_domain(range(self.__nvalues))
-
-        for datum in data:
-            assert 0 <= datum < self.__nvalues
-            tree : Node[Datum] = build_tree(frequencies)
-            codes : dict[Datum, list[Bit]] = build_codebook(tree)
-            yield from codes[datum]
-            frequencies.increment(datum)
-
-    def decode(self, bits : Iterable[Bit]) -> Data:
-        frequencies : FrequencyTable[Datum] = FrequencyTable.with_domain(range(self.__nvalues))
-        current_node = build_tree(frequencies)
-        end_reached = False
-
-        for bit in bits:
-            assert isinstance(current_node, Branch)
-            current_node = current_node.left if bit == 0 else current_node.right
-            if isinstance(current_node, Leaf):
-                yield current_node.datum
-                frequencies.increment(current_node.datum)
-                current_node = build_tree(frequencies)
--- python/tests/tests.py
+++ python/tests/tests.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('..')
+
 from huffman import TreeEncoding, HuffmanEncoding
 from huffman_ga import GrowingTreeAdaptiveHuffmanEncoding
 from huffman_fta import FullTreeAdaptiveHuffmanEncoding

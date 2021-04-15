4305c8bf97ef2b2be509049c40156e0cbcc6760e
Frederic Vogels <frederic.vogels@ucll.be>
2021-04-15 12:35:54 +0200

    test splitting

--- python/tests/tests.py
+++ python/tests/tests.py
@@ -17,27 +17,6 @@ from bit_grouper import BitGrouperEncoding
 from oracles import MemoryOracle, MarkovOracle, ConstantOracle, RepeatOracle
 
 
-def test_decode_data():
-    def check(expected, data, tree):
-        expected = [*expected]
-        actual = list(decode_data(iter(data), tree))
-        assert expected == actual, f'actual={repr(actual)}, expected={expected}'
-    def B(left, right):
-        return Branch(left, right)
-
-    a = Leaf('a')
-    b = Leaf('b')
-    c = Leaf('c')
-
-    yield check, 'a', [0], B(a, b)
-    yield check, 'aa', [0, 0], B(a, b)
-    yield check, 'ab', [0, 1], B(a, b)
-    yield check, 'a', [0, 0], B(B(a, b), c)
-    yield check, 'b', [0, 1], B(B(a, b), c)
-    yield check, 'c', [1], B(B(a, b), c)
-    yield check, 'abc', [0,0,0,1,1], B(B(a, b), c)
-
-
 def test_huffman_encoding():
     def check(data):
         encoding = HuffmanEncoding(256)

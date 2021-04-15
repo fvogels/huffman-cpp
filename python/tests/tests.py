259e6ee10a9f0d5c462db962a08476b1db371575
Frederic Vogels <frederic.vogels@ucll.be>
2021-04-15 12:16:09 +0200

    test splitting

--- python/tests/tests.py
+++ python/tests/tests.py
@@ -17,21 +17,6 @@ from bit_grouper import BitGrouperEncoding
 from oracles import MemoryOracle, MarkovOracle, ConstantOracle, RepeatOracle
 
 
-
-
-def test_padding():
-    def check(expected, xs, length, padder):
-        assert expected == pad(xs, length, padder)
-    yield check, [], [], 0, 0
-    yield check, [0], [], 1, 0
-    yield check, [0, 0], [], 2, 0
-    yield check, [1, 1], [], 2, 1
-    yield check, [5, 1], [5], 2, 1
-    yield check, [5, 5], [5, 5], 2, 1
-    yield check, [5, 5, 3], [5, 5, 3], 2, 1
-    yield check, [5, 5, 3, 0, 0], [5, 5, 3], 5, 0
-
-
 def test_group():
     def check(expected, xs, group_size):
         assert expected == group(xs, group_size)
